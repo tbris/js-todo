78550bf96115369a1ef34cea119ad3116c814c97
Erik Tejeda <etrq@protonmail.com>
2021-04-06 16:26:57 -0500

    Fix sidebar responsiveness

--- src/domSidebar.js
+++ src/domSidebar.js
@@ -4,7 +4,7 @@ function toggleNav(e) {
   let sidebarWidth = Containers.sidebar.style.width
   let factor = "0";
   if (!sidebarWidth || sidebarWidth == "0px") {
-    factor = window.screen.width > 890 ? "20%" : "65%";
+    factor = window.innerWidth > 890 ? "20%" : "65%";
   }
   Containers.sidebar.style.width = factor;
 }
